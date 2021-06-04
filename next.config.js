05ffe103bbedf86ff9a1f7d6e89a5e40c8277654
JulioAvalos <julioavalos2@msn.com>
2021-06-03 19:52:19 -0600

    running mongodb queries from inside API Routes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   env: {
     BACKEND_URL: process.env.BACKEND_URL,
+    USERNAME: process.env.USERNAME_DB,
+    PASSWORD: process.env.PASSWORD_DB,
+    DATABASE: process.env.DATABASE,
   },
 };
--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,13 +1,25 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const userMail = req.body.email;
-    if(!userMail || !userMail.includes('@')) {
-      res.status(422).json({ messsage: "invalid email address."});
+    if (!userMail || !userMail.includes("@")) {
+      res.status(422).json({ messsage: "invalid email address." });
       return;
-    } 
+    }
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@cluster0.nmhiv.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`
+    );
+
+    const db = client.db();
+
+    await db.collection("emails").insertOne({ email: userMail });
+
+    client.close();
+
     res.status(201).json({ messsage: "Signed up!" });
   } else {
-    res.status(200).json({ messsage: "OK"});
+    res.status(200).json({ messsage: "OK" });
   }
 }
 
